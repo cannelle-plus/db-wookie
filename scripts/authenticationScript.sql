09d23d22534015b39d1362437420ec49297a6551
Yoann <artissae@hotmail.com>
2014-07-13 15:17:33 +0200

    Adding proejctionsTables GamesList and Users

--- scripts/authenticationScript.sql
+++ scripts/authenticationScript.sql
@@ -4,6 +4,8 @@ BEGIN TRANSACTION;
 
 CREATE TABLE Authentication (username text primary key not null, password text not null);
 CREATE TABLE Session (tokenId text primary key not null, expirationDate text not null, userName text not null);
+CREATE TABLE Users (userName text primary key not null, LastName text not null, FirstName text not null);
+CREATE TABLE GamesList (gameId text primary key not null, gameDate text not null, gameLocation text not null, players text not null);
 
 
 COMMIT;
--- scripts/authenticationScript.tests.sql
+++ scripts/authenticationScript.tests.sql
@@ -2,9 +2,18 @@
 BEGIN TRANSACTION;
 
 --sql for authentication
-Insert into Authentication VALUES ('admin', 'yogolo49');
+Insert into Authentication VALUES ('yoann', 'yogolo49');
+Insert into Authentication VALUES ('aziz', 'aziz');
+Insert into Authentication VALUES ('julien', 'julien');
 
 --sql for session
-Insert into Session VALUES ('88085239-6f0f-48c6-b73d-017333cb99ba', '2099-12-31 23:59:59.999', 'admin');
+Insert into Session VALUES ('88085239-6f0f-48c6-b73d-017333cb99ba', '2099-12-31 23:59:59.999', 'yoann');
+Insert into Session VALUES ('88085239-6f0f-48c6-b73d-017333cb99wd', '2099-12-31 23:59:59.999', 'aziz');
+Insert into Session VALUES ('88085239-6f0f-48c6-b73d-017333cb99we', '2099-12-31 23:59:59.999', 'julien');
+
+--sql for users
+Insert into Users VALUES ('yoann', 'Remy', 'Yoann');
+Insert into Users VALUES ('aziz', 'El Bouzidi', 'Aziz');
+Insert into Users VALUES ('julien', 'Fouquet', 'Julien');
 
 COMMIT;
