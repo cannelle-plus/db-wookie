fac58511c6fe019ea6c6c65e077b92e425312d8e
Yoann <artissae@hotmail.com>
2014-09-21 21:26:17 +0200

    projection  at least once table added

--- scripts/authenticationScript.sql
+++ scripts/authenticationScript.sql
@@ -6,5 +6,7 @@ BEGIN TRANSACTION;
 CREATE TABLE Users (userId text primary key not null, userName text not null);
 CREATE TABLE GamesList (id text primary key not null, version text not null, name text not null, ownerId text not null,ownerUserName text not null, startDate text not null, location text not null, players text not null, nbPlayers text not null, maxPlayers text not null);
 
+CREATE TABLE Projections (name text not null, messageIdProcessed text not null);
+
 
 COMMIT;
