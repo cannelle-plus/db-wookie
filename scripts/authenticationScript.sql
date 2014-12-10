ec2471b047d7cdac3b2460043a054597a08ee9e5
Yoann <artissae@hotmail.com>
2014-12-10 21:00:28 +0100

    Changing bearName column

--- scripts/authenticationScript.sql
+++ scripts/authenticationScript.sql
@@ -4,7 +4,7 @@ BEGIN TRANSACTION;
 
 
 CREATE TABLE Users (socialId text not null, bearId text not null);
-CREATE TABLE Bears (bearId text not null, bearName text not null, bearAvatarId text not null);
+CREATE TABLE Bears (bearId text not null, bearUsername text not null, bearAvatarId text not null);
 CREATE TABLE GamesList (id text primary key not null, version text not null, name text not null, ownerId text not null,ownerBearName text not null, startDate text not null, location text not null, players text not null, nbPlayers text not null, maxPlayers text not null);
 
 CREATE TABLE Projections (name text not null, messageIdProcessed text not null);
